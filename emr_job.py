af0d31782704f1e718db62a1e4b6de0d3c8c502f
Smeet97Kathiria <50065223+Smeet97Kathiria@users.noreply.github.com>
2021-06-11 18:08:04 -0400

    Update emr_job.py

--- emr_job.py
+++ emr_job.py
@@ -60,7 +60,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 
-EMR_CLUSTER_ID = 'j-3MM8L3MY7RQRQ'
+EMR_CLUSTER_ID = 'j-3689DCDPZUI9V'
 INPUT_DATA_CSV = 's3://wcd-data-engineering-pipeline/source/banking.csv'
 OUTPUT_DIRECTORY = 's3://wcd-data-engineering-pipeline/output'
 ARTIFACT_JAR = 's3://wcd-data-engineering-pipeline/spark-engine_2.11-0.0.1.jar'
